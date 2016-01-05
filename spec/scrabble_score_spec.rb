aa35a636f404a8f6bfd8a9f3a90c26926ab5ddeb
Kyle Langley and Riley Starnes <altck8@gmail.com>
2016-01-05 11:40:06 -0800

    return false if input is letter

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,6 +1,12 @@
 require('rspec')
 require('scrabble_score')
 
+describe('String#include_number') do
+  it('returns false if letter') do
+    expect('d'.include_number()).to(eq(false))
+  end
+end
+
 describe("String#scrabble_score") do
   it('returns a scrabble score for a letter') do
     expect("i".scrabble_score()).to(eq(1))
