bacafb828202125379d94c746093e2628f76432d
Kyle Langley and Riley Starnes <rstarnes9@gmail.com>
2016-01-05 09:25:54 -0800

    returns higher score for letter

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -3,6 +3,7 @@ class String
     alphabet_score = Hash.new()
     alphabet_score.store("AEIOULNRST", 1)
     alphabet_score.store("DG", 2)
+    alphabet_score.store("BCMP", 3)
 
     alphabet_score.keys.each do |score_key|
       score_string = score_key.split(//)
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -8,4 +8,7 @@ describe("String#scrabble_score") do
   it('returns a scrabble score for a more valuable letter') do
     expect('d'.scrabble_score()).to(eq(2))
   end
+  it('returns a scrabble score for an even more valued letter') do
+    expect('b'.scrabble_score()).to(eq(3))
+  end
 end
