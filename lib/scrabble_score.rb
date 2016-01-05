ffd426c1bb096a700b62155617656b5d2c467084
Kyle Langley and Riley Starnes <altck8@gmail.com>
2016-01-05 09:17:58 -0800

    return a higher scrabble score for a higher valued letter

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -2,6 +2,7 @@ class String
   define_method(:scrabble_score) do
     alphabet_score = Hash.new()
     alphabet_score.store("AEIOULNRST", 1)
+    alphabet_score.store("DG", 2)
 
     alphabet_score.keys.each do |score_key|
       score_string = score_key.split(//)
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,4 +5,7 @@ describe("String#scrabble_score") do
   it('returns a scrabble score for a letter') do
     expect("i".scrabble_score()).to(eq(1))
   end
+  it('returns a scrabble score for a more valuable letter') do
+    expect('d'.scrabble_score()).to(eq(2))
+  end
 end
